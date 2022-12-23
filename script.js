6ed4b23127b54108424a882d1d972dedfc236231
darlanbbs <109982294+darlanbbs@users.noreply.github.com>
2022-12-23 19:08:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let min = document.querySelector('.min')
+let temp = document.querySelector('.temp')
+let max = document.querySelector('.max')
+let cidade = document.querySelector('.cidade')
+let form = document.querySelector('.forms')
+let loader = document.querySelector('.loader')
+let hide = document.querySelector('.hide')
+let noFound = 'Cidade não encontrada'
+let graus = document.querySelector('.graus')
+form.addEventListener('submit',async (e)=>{
+    var value = document.querySelector('.input').value
+    if(value == ''){
+        e.preventDefault()
+        limpar()
+        
+    
+}else{
+    loading()
+    e.preventDefault()
+    let site = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(value)}&units=metric&lang=pt_br&appid=d06cdb298fafc83c520d5ab677fc477e`
+    let response = await fetch(site)
+    let json = await response.json()
+    
+
+    let jsonObj = {
+        tempMinima:json.main.temp_min,
+        temp:json.main.temp,
+        tempMax:json.main.temp_max,
+        nome:json.name,
+        country:json.sys.country,
+        cod:json.cod
+    }
+    hide.style.display = 'flex'
+    graus.style.display = 'flex'
+    min.innerHTML = jsonObj.tempMinima
+    temp.innerHTML = jsonObj.temp
+    max.innerHTML = jsonObj.tempMax
+    cidade.innerHTML = `${jsonObj.nome},${jsonObj.country}`
+    console.log(jsonObj)
+    limparLoading()
+}
+
+})
+
+function limpar(){
+    hide.style.display = 'none'
+    graus.style.display = 'none'
+    cidade.innerHTML = noFound
+}
+function loading(){
+    loader.style.display = 'flex'
+}
+function limparLoading(){
+    loader.style.display = 'none'
+}
